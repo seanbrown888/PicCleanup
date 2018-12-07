61c185959b39c3d7d5a1c786837d89832bc93254
Sean Brown <seanbrown888@gmail.com>
2018-12-07 09:20:09 -0700

    A little test class cleanup

--- TestPicCleanup/TestPicCleanupBL.cpp
+++ TestPicCleanup/TestPicCleanupBL.cpp
@@ -1,10 +1,10 @@
 
 #include <vector>
-
 #include "gtest/gtest.h"
 
 #include "../PicCleanupLib/PictureFileInfo.h"
 #include "../PicCleanupLib/PicCleanupBL.h"
+
 #include "M_PictureFileDiskCleaner.h"
 #include "M_RecursiveFileListingProvider.h"
 
@@ -13,34 +13,22 @@ using ::testing::Return;
 
 namespace {
 
-    // The fixture for testing class Foo.
     class PicCleanupBLTest : public ::testing::Test {
     protected:
         
 
-        PicCleanupBLTest() : bl_(fileListingProvider_, diskCleaner_) {
-            // You can do set-up work for each test here.
-            
+        PicCleanupBLTest() : bl_(fileListingProvider_, diskCleaner_) {   
         }
 
         ~PicCleanupBLTest() override {
-            // You can do clean-up work that doesn't throw exceptions here.
         }
 
-        // If the constructor and destructor are not enough for setting up
-        // and cleaning up each test, you can define the following methods:
-
         void SetUp() override {
-            // Code here will be called immediately after the constructor (right
-            // before each test).
         }
 
         void TearDown() override {
-            // Code here will be called immediately after each test (right
-            // before the destructor).
         }
 
-        // Objects declared here can be used by all tests in the test case for Foo.
         M_RecursiveFileListingProvider fileListingProvider_;
         M_PictureFileDiskCleaner diskCleaner_;
 
@@ -52,23 +40,23 @@ namespace {
     // Then the duplicates will be moved to the duplicate folder and original to original folder
     TEST_F(PicCleanupBLTest, DoesSomething) {
       
+        // Setup test data
         PictureFileInfo oneFile("file1.png", "c:\\pictures\\imported");
         PictureFileInfo twoFile("file2.png", "c:\\pictures\\imported");
         PictureFileInfo duplicateOfOneFile("file1.png", "c:\\pictures\\otherImportedDirectory");
-
         std::vector<PictureFileInfo> testInputData = {oneFile, twoFile, duplicateOfOneFile};
 
+        // Ensure test data is used
         ON_CALL(fileListingProvider_, filesInDirectory(_, _)).WillByDefault(Return(testInputData));
 
+        // Setup expected output
         EXPECT_CALL(diskCleaner_, moveFile("c:\\pictures\\imported\\file1.png", "c:\\pictures\\originals\\file1.png"));
         EXPECT_CALL(diskCleaner_, moveFile("c:\\pictures\\imported\\file2.png", "c:\\pictures\\originals\\file2.png"));
         EXPECT_CALL(diskCleaner_, moveFile("c:\\pictures\\otherImportedDirectory\\file1.png", "c:\\pictures\\duplicates\\file1-otherImportedDirectory.png"));
 
-    }
-
-    // Tests that Foo does Xyz.
-    TEST_F(PicCleanupBLTest, DoesXyz) {
-        // Exercises the Xyz feature of Foo.
+        // Perform action to get the expected output
+        // ACTION - not actually sure what action to call yet, good place to start ;)
+        //        - Another good place that could be started in parallel (if there was more than one of me, concretes of our Mocks from above
     }
 
 }  // namespace
