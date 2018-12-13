498f243a7a1030584e722d214fb4459617c1a52a
Sean Brown <seanbrown888@gmail.com>
2018-12-13 09:47:23 -0700

    First green for File mover

--- PicCleanupLib/FileOnDiskMover.cpp
+++ PicCleanupLib/FileOnDiskMover.cpp
@@ -1,6 +1,10 @@
 #include "stdafx.h"
 #include "FileOnDiskMover.h"
 
+#include <filesystem>
+#include <string>
+
+namespace fs = std::experimental::filesystem;
 
 FileOnDiskMover::FileOnDiskMover()
 {
@@ -13,4 +17,5 @@ FileOnDiskMover::~FileOnDiskMover()
 
 void FileOnDiskMover::moveFile(const std::string & oldFilenameWithPath, const std::string & newFileNameWithPath)
 {
+    fs::rename(oldFilenameWithPath, newFileNameWithPath);
 }
